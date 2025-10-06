8ebb5e2896c49cd9f7f51d22416e1062ccabe0f8
Boby Gupta <bobygupta603@gmail.com>
2025-10-06 18:15:32 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-let chance="X";
-let game=["","","","","","","","",""];
-const main=document.querySelector("#container");
-main.addEventListener("click",(e)=>{
+let chance="X";          //Initialize Variable Chance as X.
+let game=["","","","","","","","",""];  //An Array to Store Value of Index.
+const main=document.querySelector("#container"); //To Select the Div through Id .
+//An Event Listner that  performs operations on click and it also includes Call Back Function as well. 
+main.addEventListener("click",(e)=>{              
 let curr=e.target;
 let name=parseInt(e.target.dataset.name);
 if(curr.innerText==""){
     curr.innerText=chance;
 game[name]=chance;
-let win=checkwin();
+let win=checkwin();   // Calling a Function 
 if(win){
     setTimeout(()=>{
     alert(`WHo chhose( ${chance} ) Player is win`);
@@ -16,7 +17,7 @@ if(win){
      });
     return;
 }
-let dra=draw();
+let dra=draw();       // Calling a Function 
 if(dra){
      setTimeout(()=>{
     alert(" OOPs Game is Draw");
@@ -28,6 +29,7 @@ if(dra){
 chance=chance=="X"?"O":"X";
 }
 });
+// To check which Player is won  either "X" or "O".
 function checkwin(){
   if(checkEqual(0,1,2,chance)||checkEqual(3,4,5,chance)||checkEqual(6,7,8,chance)){
     return true;
@@ -40,12 +42,14 @@ function checkwin(){
   }  
   return false;
 }
+//It is also a call back function so that we can reuse this .
 function checkEqual(a,b,c,chance){
     if(game[a]==chance && game[b]==chance && game[c]==chance){
         return true;
     }
     return false;
 }
+//After Game has finished , This function will empty Tic Tac Toe .
 function reset(){
     game=["","","","","","","","",""];
     let cells=main.children;
@@ -53,6 +57,7 @@ function reset(){
         cells[i].innerText="";
     }
 }
+//This function ensures the game is tie over on not . 
 function draw(){
     for(let i=0;i<game.length;i++){
         if(game[i]==""){
@@ -61,4 +66,5 @@ function draw(){
 
     }
     return true;
+
 }
